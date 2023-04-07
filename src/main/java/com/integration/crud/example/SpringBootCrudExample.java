8cdc05100da8d8a0e8f4e663d36b79c58ee97d51
jaecayy <jiteshkhatri2000@gmail.com>
2023-04-07 16:55:37 +0530

    Initial commit

---
+++ src/main/java/com/integration/crud/example/SpringBootCrudExample.java
@@ -0,0 +1,16 @@
+package com.integration.crud.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudExample {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootCrudExample.class, args);
+    }
+
+}
---
+++ src/main/java/com/integration/crud/example/entity/Product.java
@@ -0,0 +1,28 @@
+package com.integration.crud.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
